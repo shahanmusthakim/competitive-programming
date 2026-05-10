54125f0766323ee8d14529e7d84c3d9b8fda3052
MD MUSTHAKIM AHMED SHAHAN <shahanmusthakim@gmail.com>
2026-05-10 20:43:05 +0600

    Add files via upload

---
+++ Math And Number Theory/A_Maximum_GCD.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/1370/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << N / 2 << endl;
+    }
+    return 0;
+}
