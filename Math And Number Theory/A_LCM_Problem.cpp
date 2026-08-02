5f35a6de08435d9dc46878f54db0dd37dfaff774
MD MUSTHAKIM AHMED SHAHAN <shahanmusthakim@gmail.com>
2026-08-02 20:59:46 +0600

    Add files via upload

---
+++ Math And Number Theory/A_LCM_Problem.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/1389/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;
+        if (2 * A <= B)
+            cout << A << " " << 2 * A << '\n';
+        else
+            cout << "-1 -1\n";
+    }
+    return 0;
+}
