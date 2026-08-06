9c3fd0f4eb38c491cd9526b26dbadd7a7f1cfe09
MD MUSTHAKIM AHMED SHAHAN <shahanmusthakim@gmail.com>
2026-08-06 15:18:08 +0600

    Add files via upload

---
+++ Bit Manipulation/OR_Permutation.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/PERMOR
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        for (int i = N; i >= 1; i--)
+        {
+            cout << i << " ";
+        }
+        cout << '\n';
+    }
+}
