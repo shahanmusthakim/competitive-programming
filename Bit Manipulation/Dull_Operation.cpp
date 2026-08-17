167ce93cf8dba65e88fec40fd906b2066beab5ea
MD MUSTHAKIM AHMED SHAHAN <shahanmusthakim@gmail.com>
2026-08-17 20:06:06 +0600

    Add files via upload

---
+++ Bit Manipulation/Dull_Operation.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/DUPLET
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << N << " " << N - 1 << '\n';
+    }
+
+    return 0;
+}
