dcd7a6bbcc8e9d5bfc13c81f6b6a0277f3da0092
MD MUSTHAKIM AHMED SHAHAN <shahanmusthakim@gmail.com>
2026-09-19 21:30:17 +0600

    Time: 0 ms (100.00%) | Memory: 10.8 MB (17.37%) - LeetSync

--- LeetCode/1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ LeetCode/1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        map<int ,int>m;
+        unordered_map<int ,int>m;
         int n=nums.size();
         for(int i=0;i<n;i++){
             m[nums[i]]++;
